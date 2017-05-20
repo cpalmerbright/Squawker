aa50c762fc86d118092b1a8f01f241819ec285dc
Caitlin Palmer-Bright <c_palmerbright@hotmail.com>
2017-05-20 12:35:09 +1000

    Can create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,27 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path, notice: 'Your post has been created.' }
+        # format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 end
